ac6fc8b95f49be970b86cb3eee9fd11e397de786
Ga2ng <gagangprakasa@gmail.com>
2025-11-24 21:23:38 +0700

    setup analytics vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -27,7 +28,10 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="bg-[#020807] text-white antialiased">{children}</body>
+      <body className="bg-[#020807] text-white antialiased">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
