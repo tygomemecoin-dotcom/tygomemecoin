27faaef51e76564ba6817e2cf4746761ded343ee
Ga2ng <gagangprakasa@gmail.com>
2025-11-24 16:35:07 +0700

    make repo public again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
           <BuyPortalSection />
           <TokenomicsSection />
           <RoadmapSection />
-          <BlueprintSection />
+          {/* <BlueprintSection /> */}
           <HowToBuySection />
           <FooterSection />
         </main>
